fac8f7148f02a8170238b019702ac4349384b8ab
Henrique Paes <henriquee.paes1@gmail.com>
2024-05-09 18:26:14 -0300

    funcionando no simulador

--- sort.s
+++ sort.s
@@ -1,7 +1,20 @@
 # sort function
 # saving registers
 # convention: x0=zero, x1=ra, sp=sp, x5-7=t0-2, x10-11=a0-1, x12-17=a2-7, x18-27=s2-11, x28-31=t3-6
-main:       
+# Posicao dos argumentos: v=a0,n=a1
+    .rodata
+.ARRAY:
+    .word 0x000000010
+    .word 0x000000012
+    .word 0x00000000F
+    .word 0x000000001
+    .word 0x00000000C
+    .word 0x000000004
+    .text
+
+main:
+    addi    a0, a0, %lo(.ARRAY)
+    addi    a1, a1, 6       
     addi    sp, sp, -20
     sw      ra,16(sp)
     sw      s6,12(sp)
@@ -9,8 +22,8 @@ main:
     sw      s4,4(sp)
     sw      s3,0(sp)
 # move parameters
-    addi    s5, a0, 0               # a0 tem a posição inicial do array
-    addi    s6, a1, 0               # a1 tem o tamanho n
+    addi    s5, a0, 0
+    addi    s6, a1, 0
 # outer loop
     addi    s3, zero, 0
 for1tst:    
@@ -29,11 +42,11 @@ for2tst:
     addi    a1,s4,0
     jal     ra, swap
 # inner loop
-    addi    s4, s4, -1
+    addi    s4, s4, -1      # j = j - 1
     jal     zero, for2tst
 # outer loop
 exit2:
-    addi    s3, s3, 1
+    addi    s3, s3, 1       # i = i + 1
     j       for1tst
 # restoring registers
 exit1:
