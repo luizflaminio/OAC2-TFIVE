df26bc0c42beb4026e032b8eff6316683b79d8d2
Henrique Paes <henriquee.paes1@gmail.com>
2024-06-22 18:38:43 -0300

    escrevendo if comportamental

---
+++ 4a_entrega/estagio_if.vhd
@@ -0,0 +1,145 @@
+---------------------------------------------------------------------------------------------------------
+---------------MOD�LO DE BUSCA - IF -------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+library work;
+use work.tipos.all;
+use work.opcode_converter.all;
+
+-- Especificaçao do estágio de BUSCA - if
+-- estágio de Busca de Instruções - if: neste estágio se encontra o PC(PC_if) (Contador de Programa) 
+-- o Registrador de Instruções ri_if,o registrador  
+-- NPC (NPC_if = PC incrementado de 4), a memória Cache de instruções - iMEM e um conjunto de informações 
+-- passadas ao estágio de decodificação-id.
+-- Essas informações sao passadas por um sinal chamado BID (Buffer para o estágio id). Este buffer é de 
+-- saído do estágio if 
+-- e de entrada no estágio id. Este estágio recebe sinais vindos de outros estágios, a saber:
+--		clock; Sinal vindo da Bancada de teste que implementa o relógio do Pipeline;
+-- 		id_hd_hazard: Sinal de controle vindo do estágio id, no módulo hd, que carrega 0's na parte do ri  
+-- 			do registrador de saída do estágio de Busca (BID) quando da ocorrência de um conflito;
+-- 		id_hd_Branch_nop:Sinal vindo do estágio id, do módulo hd, que indica inserção de NoP devido  
+--          a desvio ou pulo;
+-- 		id_PC_Src: Sinal vindo do estágio id que define a seleçao do multiplexador da entrada 
+--		do registrador PC;
+-- 		id_Jump_PC: Sinal vindo do estágio id com o endereço destino ("target") dos Pulos ou desvios  
+--			a serem realizados.
+--		keep_simulating: sinal que indica continuação (true) ou parada (false) da simulação.
+-- O BID possui 64 bits alocados da seguinte forma: o ri_if nas posições de 0 a 31 e o PC_if de 32 a 63.
+
+entity estagio_if is
+    generic(
+        imem_init_file: string := "imem.txt" -- Nome do arquivo com o conteúdo da memoria de programa
+    );
+    port(
+        -- Entradas
+        clock           : in std_logic; -- Base de tempo vinda da bancada de teste
+        id_hd_hazard    : in std_logic; -- Sinal de controle que carrega 0's na parte do RI do registrador de saída BID
+        id_Branch_nop   : in std_logic; -- Sinal que determina inserção de NOP- desvio ou pulo
+        id_PC_Src       : in std_logic; -- Seleção do mux da entrada do PC
+        id_Jump_PC      : in std_logic_vector(31 downto 0) := x"00000000"; -- Endereço do Jump ou desvio realizado
+        keep_simulating : in boolean; -- Sinal que indica a continuação da simulação
+        -- Saída
+        BID             : out std_logic_vector(63 downto 0) := x"0000000000000000" -- Reg. de saída if para id
+    );
+end entity;
+
+architecture behav of estagio_if is
+    
+    component ram is
+        generic(
+            address_bits    : integer := 32; -- Número de bits de endereço da memória
+            size            : integer := 4096; -- Tamanho da memória em bytes
+            ram_init_file   : string := "imem.txt" -- Arquivo que contém o conteúdo da memória
+        );
+        port (
+            -- Entradas
+            clock   : in std_logic; -- Base de tempo, memória síncrona para escrita
+            write   : in std_logic; -- Sinal de escrita na memória
+            address : in std_logic_vector(address_bits-1 downto 0); -- Entrada de endereço da memória
+            data_in : in std_logic_vector(address_bits-1 downto 0); -- Entrada de dados na memória
+            
+            -- Saída
+            data_out: out std_logic_vector(address_bits-1 downto 0) -- Saída de dados da memória
+        );
+    end component;
+    
+    signal s_reset: std_logic;
+    signal s_instruction : std_logic_vector(31 downto 0);
+    signal ri_if : std_logic_vector(31 downto 0);
+    signal s_PC  : std_logic_vector(31 downto 0) := x"00000000";
+    signal s_pc_plus_4  : std_logic_vector(31 downto 0) := x"00000000";
+    signal s_pc_mux  : std_logic_vector(31 downto 0);
+    signal COP_IF : instruction_type := NOP;
+
+begin
+    s_reset <= '0' when keep_simulating else '1';
+    -- Como foi inserido o endereço de interrupção/exceção?
+    -- O endereço de interrupção é uma das entradas do mux que determina qual o PC a ser usado, é selecionado a partir do sinal id_Branch_nop
+    -- Já o endereço de exceção foi implementado como sujerido no enunciado, o endereço 0x00000400 e é selecionado pelo sinal id_PC_src
+
+    s_pc_plus_4 <= std_logic_vector(to_unsigned(to_integer(unsigned(s_PC)) + 4, s_pc_plus_4'length));
+
+    pc_source_process: process(id_Branch_nop, id_PC_src, s_pc_plus_4)
+        begin
+            if(id_Branch_nop = '1') then
+                s_pc_mux <= id_Jump_PC;
+            elsif(id_PC_src = '1') then
+                s_pc_mux <= x"00000400";
+            else
+                s_pc_mux <= s_pc_plus_4;
+            end if;
+    end process;
+
+    -- Como se implementou a preservação do valor do PC?
+    -- O valor de PC só atualiza se id_hd_hazard = '1'
+
+    pc_process: process(clock)
+    begin
+        if(rising_edge(clock)) then
+            if(id_hd_hazard = '0') then
+                S_PC <= s_pc_mux;
+            else
+                S_PC <= S_PC;
+            end if;
+        end if;
+    end process;
+
+    -- Como se implementou a inserção de NOPs?
+    -- Se o sinal de hazard estiver ativo, passa-se adiante a inserção de bolhas
+
+    select_instruction: process(id_hd_hazard, s_instruction)
+        begin
+            if(id_hd_hazard = '0') then
+                ri_if <= s_instruction;
+            else
+            ri_if <= x"00000000"; -- NOP
+            end if;
+        end process;
+    
+    COP_IF <= decode(ri_if);
+
+    bid_process: process(clock)
+    begin
+        if(id_hd_hazard = '0' and rising_edge(clock)) then
+            BID <= s_PC & ri_if;
+        end if;
+    end process;
+
+    imem: ram
+        generic map(
+            address_bits    => 32,
+            size            => 4096,
+            ram_init_file   => imem_init_file
+        )
+        port map(
+            clock       => clock,
+            write       => '0',
+            address     => s_PC,
+            data_in     => (others => '0'),
+            data_out    => s_instruction
+        );
+
+end behav;
---
+++ 4a_entrega/tb_if_nosso.vhd
@@ -0,0 +1,286 @@
+----------------------------------------------------------------------------------------------------------------------------------
+-------------------------------Bancada de teste do estagio de Busca - rotina swap modificada--------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_signed.all;
+
+use std.env.stop;
+ 
+entity tb_if is
+end entity; 
+
+architecture tb of tb_if is
+component estagio_if is
+    generic(
+        imem_init_file: string := "imem.txt"
+    );
+    port(
+        --Entradas
+        clock 			: in  	std_logic;	-- Base de tempo vinda da bancada de teste
+        id_hd_hazard	: in 	std_logic;	-- Sinal de controle que carrega 0's na parte do RI do registrador de sa�da do if_stage
+				id_Branch_nop	: in 	std_logic;	-- Sinal que indica inser4�ao de NP devido a desviou pulo
+				id_PC_Src		: in 	std_logic;	-- Sele�ao do mux da entrada do PC
+				id_Jump_PC		: in 	std_logic_vector(31 downto 0);			-- Endereco do Jump ou desvio realizado
+				keep_simulating	: in	Boolean := True;
+			
+			-- Saida
+        BID				: out 	std_logic_vector(63 downto 0) := x"0000000000000000"	--Registrador de sa�da do if_stage-if_id
+    );
+end component;
+
+	constant clock_period	  : time 		:= 10 ns;
+	signal clock			      : std_logic := '1';
+	signal BID				      : std_logic_vector (63 downto 0); 
+	signal id_hd_hazard		  : std_logic;
+	signal id_Branch_nop	  : std_logic;
+	signal id_PC_Src		    : std_logic;
+	signal id_Jump_PC		    : std_logic_vector(31 downto 0);
+	signal Keep_simulating	: boolean;
+
+begin
+    clock <= not clock after clock_period / 2;
+
+    DUT: estagio_if 
+        generic map (
+            imem_init_file => "imem.txt"
+        ) port map (
+          --Entradas
+          clock           => clock,			-- Base de tempo vinda da bancada de teste
+          id_hd_hazard		=> id_hd_hazard,	-- Sinal de controle que carrega 0's na parte do RI do registrador de sa�da do if_stage
+          id_Branch_nop		=> id_Branch_nop,	-- Sinal que indica inser4�ao de NP devido a desviou pulo
+          id_PC_Src			  => id_PC_Src,		-- Sele�ao do mux da entrada do PC
+          id_Jump_PC		  => id_Jump_PC,		-- Endere�o do Jump ou desvio realizado
+          keep_simulating	=> Keep_simulating,	-- Continue a simula�ao
+
+          -- Saida
+          BID  => BID  --Registrador de sa�da do if_stage-if_id
+        );
+
+    stim: process is
+    begin
+        id_hd_hazard 	  <= '0';			    -- Nao insere NOP
+				id_Branch_nop 	<= '0';		   	  -- Nao sinaliza Jump
+				id_PC_Src 		  <= '0';		      -- Nao é instrucao de desvio ou pulo
+				id_Jump_PC 		  <= x"00000000"; -- Se tiver que pular o próximo endere�o ser� x"00000000"
+				Keep_simulating <= true;		    -- Mantenha simulacao ativa 
+		
+		wait for clock_period;		 -- ciclo 1 = 0 ns espera um ciclo de relogio 
+		
+		-- Situação no instante 10 ns
+		report "Testando se instrucao na posicao 1 esta correta";
+		assert BID(31 downto 00) = x"00000513" severity error;	 	-- Testa se esta instrucao eh: addi	a0,	zero,0
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	 	  -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000000"  severity error;	 	-- Testa se PC = x"00000000
+		report "PC_id = " & to_hex_string(BID(63 downto 32));	 	  -- Imprime o valor que esta no PC_id
+		report " " ;											 	-- Pula uma linha no conslole de sa�da
+	   
+		-- Situação no instante 20 ns
+		wait for clock_period;		 -- ciclo 2 = 20 ns	  leitura da 2a. instrucao do swap
+		report "Testando se instrucao na posicao 2 (em palavras) esta correta";
+		assert BID(31 downto 00) = x"00300593" severity error; 	 	-- Testa se esta instrucao eh: addi	a0,	zero,3
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	 	  -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";					 	  
+		assert BID(63 downto 32) = x"00000004" severity error;	 	-- Testa se PC = x"00000004"
+		report " PC_id = " & to_hex_string(BID(63 downto 32)); 	 	-- Imprime o valor que esta no PC		o
+		report " " ;											 	-- Pula uma linha no console de saida
+		
+        -- Situação no instante 30 ns
+		wait for clock_period;		  -- ciclo 3 = 30 ns	leitura da 3a. instrucao do swap
+		report "Testando se instrucao na posicao 3 esta correta";
+		assert BID(31 downto 00) = x"00259293" severity error;	 -- Testa se a instrucao eh: slli t0, a1, 2 
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000008" severity error;	 -- Testa se PC = x"00000008"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida
+		
+		-- Situação no instante 40 ns
+		wait for clock_period;		  -- ciclo 4 = 40 ns	leitura da 4a. instrucao do swap
+		report "Testando se instrucao na posicao 4 esta correta";
+		assert BID(31 downto 00) = x"005502B3" severity error; 	 -- Testa se a instrucao eh: add t0,a0,t0
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"0000000C" severity error; 	 -- Testando se PC = x"0000000C"
+		report " PC_id = " & to_hex_string(BID(63 downto 32)); 	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida
+		
+		-- Situação no instante 50 ns
+		wait for clock_period;		  -- ciclo 5 = 50 ns	leitura da 5a. instrucao do swap
+		report "Testando se instrucao na posicao 5 esta correta";
+		assert BID(31 downto 00) = x"0002A303" severity error;	 -- Testa se a instrucao eh: lw t1, 0(t0)
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000010" severity error;	 -- Testa se PC = x"00000010"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida
+		
+		-- Situação no instante 60 ns
+		wait for clock_period;		   -- ciclo 6 = 60 ns	leitura da 6a. instrucao do swap
+		report "Testando se instrucao na posicao 6 esta correta";
+		assert BID(31 downto 00) = x"0042A383" severity error;	 -- Testa se a instrucao eh: lw t2, 4(t0)
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000014" severity error;	 -- Testa se PC = x"00000014"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida
+		
+		 -- Situação no instante 70 ns
+		wait for clock_period;		   -- ciclo 7 = 70 ns	leitura da 7a. instrucao do swap
+		report "Testando se instrucao na posicao 7 esta correta";
+		assert BID(31 downto 00) = x"0072A023" severity error;	 -- Testa se a instrucao eh: sw t2, 0(t0) 
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000018" severity error; 	 -- Testa se PC = x"00000018"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida
+		
+		-- Situação no instante 80 ns
+		wait for clock_period;		   -- ciclo 8 = 80 ns	leitura da 8a. instrucao do swap
+		report "Testando se instrucao na posicao 8 esta correta";
+		assert BID(31 downto 00) = x"0062A223" severity error;	 -- Testa se a instrucao eh: sw t1, 4(t0)
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"0000001C" severity error;	 -- Testa se PC = x"0000001C"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida
+		
+		-- Situação no instante 90 ns
+		wait for clock_period;		   -- ciclo 9 = 90 ns	leitura da 9a. instrucao do swap
+		report "Testando se instrucao na posicao 9 esta correta";
+		assert BID(31 downto 00) = x"00001013" severity error;	 -- Testa se a instrucao eh: slli zero, zero, 0 (NOP)
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000020" severity error; 	 -- Testa se PC = x"00000020"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida 
+		
+		
+		id_Jump_PC 		<= x"00000040"; -- Se tiver que pular o proximo endereco sera x"00000040"
+		wait for clock_period/2;
+		report "Definido endereco de jump para 0x00000040";
+		-- Situação no instante 95 ns
+		
+		
+		-- Situação no instante 100 ns
+		wait for clock_period/2;   		 -- ciclo 10 = 100 ns	 Teste de desvio inserindo endereco destino no PC 
+		report "Testando se instrucao na posicao 10 esta correta";
+		assert BID(31 downto 00) = x"00001013" severity error; 	 -- A instrucao ainda eh:	slli zero,zero, 0
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000024" severity error;	 -- Testa se PC = x"00000024"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida
+		
+		-- Situação no instante 105 ns
+		id_Branch_nop <= '1';	  -- ativa insercao de NOP no ri_if e BID
+		wait for clock_period/2;
+		report "Sinal de JUMP ligado";
+		
+		-- Situação no instante 110 ns
+		wait for clock_period/2;		  -- ciclo 11 = 110 ns	 Teste de desvio lendo o endereco destino no PC
+		report "Testando se instrucao na posicao 11 esta correta";
+		assert BID(31 downto 00) = x"00001013" severity error; 	 -- A instrucao ainda eh:	slli zero,zero, 0
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000028" severity error; 	 -- Testa se PC = x"00000028"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida
+		
+		-- Situação no instante 115 ns
+		id_Branch_nop <= '0';	  -- ativa insercao de NOP no ri_if e BID
+		wait for clock_period/2;
+		report "Sinal de JUMP desligado";
+		
+		-- Situação no instante 120 ns
+		wait for clock_period/2;		  -- ciclo 12 = 120 ns	 Teste se esta inserindo NOP no ri e BID
+		report "Testando se instrucao na posicao 17 esta correta";
+		assert BID(31 downto 00) = x"000001013" severity error; 	 -- A instrucao eh:	slli zero,zero, 0
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	 	-- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000040" severity error; 	 	-- Testa se PC = x"00000040"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 	-- Imprime o valor que esta no PC
+		report " " ;											 	-- Pula uma linha no console de saida	
+		
+		-- Situação no instante 125 ns
+		id_hd_hazard <= '1';	 -- Desativa insercao de NOP
+		wait for clock_period/2;
+		report "Sinal de HAZARD ligado";
+		
+		-- Situação no instante 130 ns
+		wait for clock_period/2;		  -- ciclo 13 = 130 ns	 Teste se est� inserindo NOP no ri e BID
+		report "Testando se NOP foi inserido corretamente";
+		assert BID(31 downto 00) = x"00000000" severity error; 	-- A instrucao eh: NOP (x"000000000" é visto como NOP)
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	 	-- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000044" severity error; 	 	-- Testa se manteve o PC = x"00000044"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 	-- Imprime o valor que esta no PC
+		report " " ;											 	-- Pula uma linha no console de saida
+		
+		-- Situação no instante 135 ns
+		id_hd_hazard <= '0';	-- Ativa deteccao de conflito 
+		wait for clock_period/2;
+		report "Sinal de HAZARD desligado";
+		
+		-- Situação no instante 140 ns
+		wait for clock_period/2;		  -- ciclo 14 = 140 ns	 Teste de deteccao de conflito com acionamento do id_hd_Hazrd
+		report "Testando se instrucao na posicao 18 esta correta";
+		assert BID(31 downto 00) = x"00000000" severity error; 	 -- A instrucao eh: NOP (x"000000000" é visto como NOP)
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000044" severity error; 	 -- Testa se manteve o PC = x"00000040"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida	
+		
+		-- Situação no instante 145 ns
+		wait for clock_period;		  -- ciclo 14 = 150 ns	 Teste de deteccao de conflito sendo desativada
+		report "Testando se instrucao na posicao 19 esta correta";
+		assert BID(31 downto 00) = x"00000000" severity error; 	 -- A instrucao eh: NOP (x"000000000" é visto como NOP)
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000048" severity error; 	 -- Testa se manteve o PC = x"00000048"
+		report " PC_if = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida	
+
+		-- Situação no instante 155 ns
+		id_PC_src <= '1';	-- Ativa deteccao de interrupção
+		wait for clock_period/2;
+		report "Sinal de HALT ligado";
+		
+		-- Situação no instante 160 ns
+		wait for clock_period/2;		  -- ciclo 15 = 160 ns	 Teste de deteccao de conflito com acionamento do id_hd_Hazrd
+		report "Testando se instrucao na posicao 20 esta correta";
+		assert BID(31 downto 00) = x"00000000" severity error; 	 -- A instrucao eh: NOP (x"000000000" é visto como NOP)
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"0000004C" severity error; 	 -- Testa se manteve o PC = x"0000004C"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida	
+
+		-- Situação no instante 165 ns
+		id_PC_src <= '0';	-- Desativa deteccao de interrupção
+		wait for clock_period/2;	
+		report "Sinal de HALT desligado";
+		
+		-- Situação no instante 170 ns
+		wait for clock_period/2;		  -- ciclo 16 = 170 ns	 Teste de deteccao de conflito com acionamento do id_hd_Hazrd
+		report "Testando se instrucao na posicao 257 esta correta";
+		assert BID(31 downto 00) = x"0000006F" severity error; 	 -- A instrucao eh: hal zero, 0; HALT
+		report "RI_if = " & to_hex_string(BID(31 downto 00));	   -- Imprime o codigo da instrucao passada para o id
+		report "Testando o valor do PC enviado para o estagio id";
+		assert BID(63 downto 32) = x"00000400" severity error; 	 -- Testa se manteve o PC = x"0000004C"
+		report " PC_id = " & to_hex_string(BID(63 downto 32));	 -- Imprime o valor que esta no PC
+		report " " ;											 -- Pula uma linha no console de saida	
+
+		-- Como foi parada a simulação quando este estágio encontrar a instrução Halt?
+		-- Inicialmente escrevemos a instrução HALT na posição de exceção na memória RAM.
+		-- É feito um assert que para a execução quando chega na instrução de HALT ao final da simulação
+
+		assert not BID(31 downto 00) = x"0000006F" severity failure; 
+
+    end process;
+
+end tb;   
