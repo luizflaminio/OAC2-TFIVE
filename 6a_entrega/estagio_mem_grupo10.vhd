43219137d1e9020e179bb98e93140b1e392be41d
Henrique-Assme <heniassme@gmail.com>
2024-07-24 15:10:58 -0300

    MEM almost done

--- 6a_entrega/estagio_mem_grupo10.vhd
+++ 6a_entrega/estagio_mem_grupo10.vhd
@@ -39,57 +39,92 @@ entity estagio_mem_grupo10 is
     );
 end entity;
 
-architecture behavioral of estagio_mem_grupo10 is
-	component ram is
-        generic(
-            address_bits    : integer := 32; -- Número de bits de endereço da memória
-            size            : integer := 4096; -- Tamanho da memória em bytes
-            ram_init_file   : string := "dmem.txt" -- Arquivo que contém o conteúdo da memória
-        );
-        port (
-            -- Entradas
-            clock   : in std_logic; -- Base de tempo, memória síncrona para escrita
-            write   : in std_logic; -- Sinal de escrita na memória
-            address : in std_logic_vector(address_bits-1 downto 0); -- Entrada de endereço da memória
-            data_in : in std_logic_vector(address_bits-1 downto 0); -- Entrada de dados na memória
-            
-            -- Saída
-            data_out: out std_logic_vector(address_bits-1 downto 0) -- Saída de dados da memória
-        );
-    end component;
+architecture behav of estagio_mem_grupo10 is
 
-	signal mem_read, mem_write, reg_write: std_logic;
-	signal mem_val, mem_data: std_logic_vector(31 downto 0);
+	component data_ram is 
+		generic(
+			address_bits		: integer 	:= 32;		  -- Bits de end. da mem�ria de dados
+			size				: integer 	:= 4099;	  -- Tamanho da mem�ria de dados em Bytes
+			data_ram_init_file	: string 	:= "dmem.txt" -- Arquivo da mem�ria de dados
+		);
+		port (
+		-- Entradas
+			clock 		: in  std_logic;							    -- Base de tempo bancada de teste
+			write 		: in  std_logic;								-- Sinal de escrita na mem�ria
+			address 	: in  std_logic_vector(address_bits-1 downto 0);-- Entrada de endere�o da mem�ria
+			data_in 	: in  std_logic_vector(address_bits-1 downto 0);-- Entrada de dados da mem�ria
+
+		-- Sa�da
+			data_out 	: out std_logic_vector(address_bits-1 downto 0)	-- Sa�da de dados da mem�ria
+		);
+	end component;
+
+	signal MemToReg		: std_logic_vector(1 downto 0); -- vai pro WB
+	signal s_RegWrite_mem	: std_logic; -- usa aqui
+	signal s_MemWrite_mem	: std_logic; -- usa aqui
+	signal s_MemRead_mem	: std_logic; -- usa aqui
+	signal s_NPC_mem		: std_logic_vector(31 downto 0); -- usa aqui e WB
+	signal s_ula_mem		: std_logic_vector(31 downto 0); -- usa aqui e WB
+	signal dado_arma_mem	: std_logic_vector(31 downto 0); -- usa aqui
+	signal rs1_mem		: std_logic_vector(4 downto 0); -- nao usa
+	signal rs2_mem		: std_logic_vector(4 downto 0); -- nao usa
+	signal s_rd_mem		: std_logic_vector(4 downto 0); -- usa aqui e WB
+	signal s_Memval_mem	: std_logic_vector(31 downto 0); -- usa aqui e WB
 
 	begin
 
-		set_COP_mem: process(clock)
-		begin
-			if rising_edge(clock) then
-				COP_wb <= COP_mem;
+	set_signals: process(BMEM)
+	begin
+		MemToReg <= BMEM(115 downto 114);
+		s_RegWrite_mem <= BMEM(113);
+		s_MemWrite_mem <= BMEM(112);
+		s_MemRead_mem <= BMEM(111);
+		s_NPC_mem <= BMEM(110 downto 79);
+		s_ula_mem <= BMEM(78 downto 47);
+		dado_arma_mem <= BMEM(46 downto 15);
+		rs1_mem <= BMEM(14 downto 10);
+		rs2_mem <= BMEM(9 downto 5);
+		s_rd_mem <= BMEM(4 downto 0);
+	end process;
+
+	-- sinais de saída desse estágio
+	RegWrite_mem <= s_RegWrite_mem;
+	MemRead_mem <= s_MemRead_mem;
+	MemWrite_mem <= s_MemWrite_mem;
+	rd_mem <= s_rd_mem;
+	ula_mem <= s_ula_mem;
+	NPC_mem <= s_NPC_mem;
+	Memval_mem <= s_Memval_mem;
+
+	process(clock)
+	begin
+		if rising_edge(clock) then
+			COP_wb <= COP_mem;
+			-- ver quais os endereços que vão cada um dos sinais dentro do BWB
+			BWB <= MemToReg; -- seletor mux WB
+			BWB <= RegWrite_mem; -- escrita regs para o ID
+			BWB <= NPC_mem; -- possivel dado para o WB
+			BWB <= ula_mem; -- possivel dado para o WB
+			if MemRead_mem = '1' then
+				BWB <= s_Memval_mem; -- possivel dado para o WB
+			else 
+				BWB <= '0';
 			end if;
-		end process;
+			BWB <= s_rd_mem; -- endereço de escrita no ID
+		end if;
+	end process;
 
-		memory_control: process(BMEM)
-			begin
-				mem_read <= BMEM(111);
-				mem_write <= BMEM(112);
-				reg_write <= BMEM(113);
-				mem_addr <= BMEM(78 downto 47);-- le-se do endereco calculado na ULA
-				mem_data <= BMEM(46 downto 15);
-			end process;
-
-		dmem: ram
-			generic map(
-				address_bits    => 32,
-				size            => 4096,
-				ram_init_file   => dmem_init_file
-			)
-			port map(
-				clock       => clock,
-				write       => mem_write,
-				address     => mem_addr,
-				data_in     => mem_data,
-				data_out    => mem_val
-			);
-end architecture behavioral;
+	ram: data_ram
+		generic map(
+			address_bits		=> 32,
+			size				=> 4099,
+			data_ram_init_file	=> data_ram_init_file
+		)
+		port map(
+			clock		=> clock,
+			write		=> MemWrite_mem,
+			address		=> ula_mem,
+			data_in		=> dado_arma_mem,
+			data_out	=> s_Memval_mem
+		);
+end architecture;
